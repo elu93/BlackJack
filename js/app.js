33331efa31032687a8c049b683e7e319db30d288
ericstotle93 <elu1993@gmail.com>
2017-12-13 14:28:59 -0500

    Fixed bug with blackjack to accurately update scores/play

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let Player = {
-    name: "",
+    name: null,
     hand: [],
     counter: 0,
     blackJack: false,
@@ -48,22 +48,56 @@ function shuffle(array) {
     return array;
 }
 
-
-//Creates Player Name
-Player.name = swal("Welcome! Who's playin' BlackJack today?", {
-    content: "input",
-}).then((value) => {
-    swal(`Welcome, ${value}! Let's see if you can make some money today.`);
-});
-
-// document.ready(function(){
-    
-// })
-//initializes page
-let myDeck = shuffle(getDeck());
 let dealerSum = 0;
 let playerSum = 0;
-dealCards();
+let myDeck = [];
+
+
+
+$(function () {
+    //Creates Player Name
+    swal("Welcome! Who's playin' BlackJack today?", {
+        content: "input",
+    }).then((value) => {
+        swal(`Welcome, ${value}! Let's see if you can make some money today.`);
+        Player.name = value;
+        changePlayerName();
+        dealCards();
+    });
+
+    $('.hit').click(function () {
+        Player.hand.push(myDeck.pop());
+        let index = Player.hand.length - 1;
+        $(`<img class="cardimage" src="${Player.hand[index].image}"/>`).appendTo('.player-cards');
+        playerSum = playerSum + Player.hand[index].value;
+        if (playerSum > 21) {
+            checkforVictory()
+            $('.hit').hide();
+            $('.stay').hide();
+            return;
+        }
+        $('.player-card-sum').text(`Player's hand: ${playerSum}`);
+    })
+
+    $('.stay').click(function () {
+        $('.dealer-card-sum').text(`Dealer's hand: ${dealerSum}`);
+        dealCardstoDealer();
+        checkforVictory();
+        $('.stay').hide();
+        $('.hit').hide();
+    })
+
+    $('.redeal').click(function () {
+        $('.cardimage').remove();
+        $('.game-text').empty();
+        $('.game-text-description').empty();
+        redealCards();
+        $('.hit').show();
+        $('.stay').show();
+    })
+});
+
+
 
 
 function giveCards(numberOfTimes) {
@@ -88,37 +122,17 @@ function showPlayerCards() {
 
 
 function dealCards() {
+    myDeck = shuffle(getDeck());
     giveCards(2);
     showPlayerCards();
-    checkforBlackJack();
+    checkPlayerForBlackJack();
     if (Player.blackJack === true || Dealer.blackJack === true) {
-        checkforVictory();
+        showBlackJackPlayer();
     }
 }
 
 
-$('.hit').click(function () {
-    Player.hand.push(myDeck.pop());
-    let index = Player.hand.length - 1;
-    $(`<img class="cardimage" src="${Player.hand[index].image}"/>`).appendTo('.player-cards');
-    playerSum = playerSum + Player.hand[index].value;
-    if (playerSum > 21) {
-        $('.game-text-description').text('Player has Busted!');
-        checkforVictory()
-        $('.hit').hide();
-        $('.stay').hide();
-        return;
-    }
-    $('.player-card-sum').text(`Player's hand: ${playerSum}`);
-})
 
-$('.stay').click(function () {
-    $('.dealer-card-sum').text(`Dealer's hand: ${dealerSum}`);
-    dealCardstoDealer();
-    checkforVictory();
-    $('.stay').hide();
-    $('.hit').hide();
-})
 
 function dealCardstoDealer() {
     $('.cardimageBack')
@@ -127,17 +141,13 @@ function dealCardstoDealer() {
     while (dealerSum < 21) {
         Dealer.hand.push(myDeck.pop());
         let index = Dealer.hand.length - 1;
+        checkDealerForBlackJack();
         if (dealerSum < 17) {
             dealerSum = dealerSum + Dealer.hand[index].value;
             $(`<img class="cardimage" src="${Dealer.hand[index].image}"/>`).appendTo('.dealer-cards');
         } else if (dealerSum < 21 && dealerSum >= 17) {
-            $('.game-text-description').text(`Dealer stops at 17.`);
-            return;
-        } else if (dealerSum === 21) {
-            $('.game-text-description').text(`The Dealer has gotten 21. Tough luck.`);
             return;
         } else if (dealerSum > 21) {
-            $('.game-text-description').text(`The Dealer busted! Get that cash $$$`);
             return;
         }
         $('.dealer-card-sum').text(`Dealer's score: ${dealerSum}`);
@@ -148,7 +158,7 @@ function checkforVictory() {
     if (playerSum <= 21 && playerSum > dealerSum) {
         playerWins();
         Player.counter = Player.counter + 1;
-        $('.player-score').text(`${playerName}'s wins: ${Player.counter}`);
+        $('.player-score').text(`${Player.name}'s wins: ${Player.counter}`);
         return;
     } else if (dealerSum <= 21 && dealerSum > playerSum) {
         dealerWins();
@@ -158,7 +168,7 @@ function checkforVictory() {
     } else if (dealerSum > 21) {
         playerWins();
         Player.counter = Player.counter + 1;
-        $('.player-score').text(`${playerName}'s wins: ${Player.counter}`);
+        $('.player-score').text(`${Player.name}'s wins: ${Player.counter}`);
         return;
     } else if (playerSum > 21) {
         dealerWins();
@@ -170,16 +180,19 @@ function checkforVictory() {
     }
 }
 
-function checkforBlackJack() {
+function checkPlayerForBlackJack() {
     if (playerSum === 21) {
         Player.blackJack = true;
-        $('.game-text').text('BLACKJACK!');
+        showBlackJackPlayer();
         $('.hit').hide();
         $('.stay').hide();
     }
+}
+
+function checkDealerForBlackJack() {
     if (dealerSum === 21) {
         Dealer.blackJack = true;
-        $('.game-text').text('Dealer got the blackjack. You mad bro?');
+        showBlackJackDealer();
         $('.hit').hide();
         $('.stay').hide();
     }
@@ -197,14 +210,7 @@ function redealCards() {
     dealCards();
 }
 
-$('.redeal').click(function () {
-    $('.cardimage').remove();
-    $('.game-text').empty();
-    $('.game-text-description').empty();
-    redealCards();
-    $('.hit').show();
-    $('.stay').show();
-})
+
 
 
 function playerWins() {
@@ -214,3 +220,17 @@ function playerWins() {
 function dealerWins() {
     swal("Dealer Wins", "Awww, the dealer took your money", "error")
 };
+
+function changePlayerName() {
+    $('.playerName').text(`${Player.name}'s Scoreboard`);
+}
+
+function showBlackJackPlayer() {
+    swal("BLACKJACK!", "WINNER WINNER CHICKEN DINNER", "success")
+    Player.counter = Player.counter + 1;
+    $('.player-score').text(`${Player.name}'s wins: ${Player.counter}`)
+}
+
+function showBlackJackDealer() {
+    swal("Tough luck", "Dealer got black jack. U mad bro?", "warning")
+}
